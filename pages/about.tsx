3082d8d41b6f53331fa9dced5305f6def760b389
Mike Gerasimenko <github@gerasimenko.me>
2022-06-20 10:39:49 +0200

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,11 +5,13 @@ export const About = (): JSX.Element => {
   return (
     <Layout
       customMeta={{
-        title: 'About - Hunter Chang',
+        title: 'About - Mike Gerasymenko',
       }}
     >
-      <h1>About Page</h1>
-      <p>Welcome to the about page</p>
+      <h1>About</h1>
+      <h2>Hey there</h2>
+      <p>This is a personal blog of Mike Gerasymenko, ukrainian🇺🇦 in Berlin.</p>
+      <p>Customer and quality focused mobile engineer in love with mobile platforms. I've been building software for quite a while: over 14 years of software development experience, 12 years of iOS. I have experience working on great products and I am excited to see them used.</p>
     </Layout>
   );
 };
