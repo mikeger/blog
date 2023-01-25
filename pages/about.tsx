3ef417573ed9b146ace384283a0ade9bbe668f82
Michael Gerasymenko <mihail@gerasimenko.me>
2023-01-25 17:05:36 +0100

    Text proofread

--- pages/about.tsx
+++ pages/about.tsx
@@ -11,7 +11,7 @@ export const About = (): JSX.Element => {
       <h1>About</h1>
       <p>This is a personal blog of Mike Gerasymenko, ukrainian🇺🇦 in Berlin.</p>
       <p>
-        Customer and quality focused mobile engineer in love with mobile
+        Customer and quality-focused mobile engineer in love with mobile
         platforms. I&apos;ve been building software for quite a while: over 14
         years of software development experience, 12 years of iOS. I have
         experience working on great products and I am excited to see them used.
